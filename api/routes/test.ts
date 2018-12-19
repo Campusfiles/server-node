fb9864c9370675d6bb70d78f8ae8d705139bfcde
Mack_Ignacio <mack_ignacio@yahoo.com>
2018-12-19 22:24:21 +0800

    Add decorator test classes

--- api/routes/test.ts
+++ api/routes/test.ts
@@ -1,9 +1,43 @@
 import { Request, Response } from "express";
+import { Injector } from "../helper/injector";
+import { Service, Log } from "../helper/decorator";
 
 export class Test {
-    testApi() {
-        return (req: Request, res: Response) => {
-            res.send({ status: "Up and running" });
-        }
-    }
+  testApi() {
+    return (req: Request, res: Response) => {
+      res.send({ status: "Up and running" });
+    };
+  }
 }
+
+// DECORATOR TEST
+
+@Service()
+class Foo {
+  @Log()
+  doFooStuff(val: any, deps?: any) {
+    const { fetch, validate, aws } = deps;
+    console.log("test", val);
+    console.log(fetch, validate, aws);
+  }
+}
+
+@Service()
+class Bar {
+  constructor(public foo: Foo) {}
+
+  doBarStuff() {
+    console.log("bar");
+  }
+}
+
+@Service()
+class Foobar {
+  constructor(public foo: Foo, public bar: Bar) {}
+}
+
+const foobar = Injector.resolve<Foobar>(Foobar);
+
+foobar.bar.doBarStuff();
+foobar.foo.doFooStuff("mack");
+foobar.bar.foo.doFooStuff("mack");
